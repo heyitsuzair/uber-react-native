f5c099d945fd38868be36fb94d5078e4855dd45c
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-27 02:51:44 +0500

    Modifications

--- components/NavigateCard/index.js
+++ components/NavigateCard/index.js
@@ -1,8 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import GooglePlacesInput from '../GooglePlaces/GooglePlacesInput';
 import NavFavourites from '../NavFavourites/NavFavourites';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ioni from 'react-native-vector-icons/Ionicons';
 
 const NavigateCard = () => {
   return (
@@ -29,6 +31,20 @@ const NavigateCard = () => {
           />
         </View>
         <NavFavourites />
+      </View>
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('')}
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+          <Icon name="car" color="white" size={16} />
+          <Text style={tw`text-center text-white`}>Rides</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+          <Ioni name="fast-food-outline" color="black" size={16} />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- components/RideOptionsCard/index.js
+++ components/RideOptionsCard/index.js
@@ -1,10 +1,69 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import tw from 'twrnc';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {useNavigation} from '@react-navigation/native';
+
+const data = [
+  {
+    id: 'Uber-X-1',
+    title: 'UberX',
+    multiplier: 1,
+    image: 'https://links.papareact.com/3pn',
+  },
+  {
+    id: 'Uber-X-2',
+    title: 'Uber XL',
+    multiplier: 1.2,
+    image: 'https://links.papareact.com/5w8',
+  },
+  {
+    id: 'Uber-X-3',
+    title: 'UberX',
+    multiplier: 1.75,
+    image: 'https://links.papareact.com/7pf',
+  },
+];
 
 const RideOptionCard = () => {
+  const navigation = useNavigation();
+
+  const [selected, setSelected] = useState(null);
+
   return (
-    <View>
-      <Text>index</Text>
+    <View style={tw`bg-white h-full`}>
+      <TouchableOpacity
+        style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        onPress={() => navigation.goBack()}>
+        <Icon name="chevron-left" color="black" size={20} />
+      </TouchableOpacity>
+      <Text style={tw`text-center py-5 text-xl`}>Select A Ride</Text>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10`}>
+            <Image
+              source={{uri: item.image}}
+              style={{width: 100, height: 100, resizeMode: 'contain'}}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`font-bold text-black`}>{item.title}</Text>
+              <Text>Time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$90</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
