2dda59f67b4ea82ec0c981571d9833b2ab21355b
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-27 00:20:13 +0500

    New Features

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,11 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import {selectOrigin} from '../../slices/navSlice';
+import {useSelector} from 'react-redux';
+import tw from 'twrnc';
 
 const index = () => {
+  const origin = useSelector(selectOrigin);
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      style={tw`flex-1`}
+      mapType="terrain"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}>
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+    </MapView>
   );
 };
 
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -10,9 +10,12 @@ import React from 'react';
 import tw from 'twrnc';
 import Icon from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
+import {useSelector} from 'react-redux';
+import {selectOrigin} from '../slices/navSlice';
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   const data = [
     {
@@ -37,8 +40,9 @@ const NavOptions = () => {
       renderItem={({item}) => (
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
-          style={tw`rounded-md p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-          <View>
+          style={tw`rounded-md p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}>
+          <View style={tw`${!origin && 'opacity-20'}`}>
             <Image
               style={{width: 120, height: 120, resizeMode: 'contain'}}
               source={item.image}
